e946aba8b7ee9424ed3af5c70e5647a6b2f45283
Luca Pavel-Andrei <pavel.luca123@gmail.com>
2023-05-09 20:08:31 +0300

    modificare baza de date

--- database.sql
+++ database.sql
@@ -1,22 +1,28 @@
 CREATE DATABASE lucrareLicenta;
 
 CREATE TABLE utilizatori(
-    utilizator_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     username VARCHAR(50),
     nume VARCHAR(100),
     prenume VARCHAR(100),
     email VARCHAR(100),
     parola VARCHAR(500),
-    rol roluri,
-    cod VARCHAR(200),
+    rol public.roluri DEFAULT 'comun'::public.roluri
     confirmat_mail boolean
 )
 
-CREATE TABLE produse(
-    produs_id SERIAL PRIMARY KEY,
-    nume VARCHAR(100),
-    marca VARCHAR(50),
-    pret NUMERIC,
-    descriere TEXT,
-    compatibilitate tip_compatibilitate
-)
+CREATE TABLE public.produse (
+    id serial primary key,
+    nume character varying(100),
+    descriere text,
+    categorie_produs public.categorie_produs DEFAULT 'placa_video'::public.categorie_produs,
+    marca public.marca DEFAULT 'Asus'::public.marca,
+    pret numeric(8,2),
+    image character varying(300),
+    compatibilitate public.tip_compatibilitate DEFAULT 'A_50'::public.tip_compatibilitate
+);
+
+INSERT INTO public.produse (nume, descriere, categorie_produs, marca, pret, imagine, compatibilitate) VALUES ('RTX 4090', 'Placa video Gigabyte seria 40', 'placa_video', 'Gigabyte', 10000, 'gigabyte4090.jpeg', 'A_50');
+INSERT INTO public.produse (nume, descriere, categorie_produs, marca, pret, imagine, compatibilitate) VALUES ('RTX 3080', 'Placa video Nvidia seria 30', 'placa_video', 'Nvidia', 4800, 'nvidia3080.jpeg', 'B_50');
+INSERT INTO public.produse (nume, descriere, categorie_produs, marca, pret, imagine, compatibilitate) VALUES ('Asus Prime', 'Placa de baza Asus', 'placa_baza', 'Asus', 800, 'asusBaza.jpeg', 'C_50');
+INSERT INTO public.produse (nume, descriere, categorie_produs, marca, pret, imagine, compatibilitate) VALUES ('Cooler NZXT', 'Sistem de racire cu apa NZXT', 'cooler', 'NZXT', 1200, 'coolerNzxt.jpeg', 'A_50');
